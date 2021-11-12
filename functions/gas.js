06e8cc2ea05b5ae3cf2baf1045da78b8b13dbde0
theblindhobo <theblindhobo@gmail.com>
2021-11-11 19:19:21 -0700

    setPresence

--- functions/gas.js
+++ functions/gas.js
@@ -32,7 +32,7 @@ const checkGasPrices = async () => {
         status: 'online',
         activities: [
           {
-            name: `FastGas: ${fastGas}`
+            name: `⚡ ${fastGas} gwei | 🚶 ${data.result.ProposeGasPrice} | 🐢 ${data.result.SafeGasPrice}`
           }
         ]
       });
