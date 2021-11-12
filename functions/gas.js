7ce51c4b2d815e8f0e2d5f1b77fc219ff84040d7
theblindhobo <theblindhobo@gmail.com>
2021-11-11 19:13:03 -0700

    setPresence

--- functions/gas.js
+++ functions/gas.js
@@ -14,21 +14,31 @@ const removeTimeout = () => {
 
 
 const checkGasPrices = async () => {
-  if(!timeout) {
-    // check price
-    await fetch(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.ETHERSCAN_API_TOKEN}`)
-      .then(response => response.json())
-      .then(data => {
-        let fastGas = data.result.FastGasPrice;
+  // check price
+  await fetch(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.ETHERSCAN_API_TOKEN}`)
+    .then(response => response.json())
+    .then(data => {
+      let fastGas = data.result.FastGasPrice;
+      if(!timeout) {
         if(fastGas < 81) {
           timeout = true;
           setTimeout(removeTimeout, 15 * 60 * 1000); // 15mins
           client.channels.cache.get(channelId.gasPrices)
             .send(`<@${theblindhobo}> Fast Gas is **${fastGas}** gwei right now!`);
         }
-      })
-      .catch(error => console.log(error.message));
-  }
+      }
+      // set presence always
+      client.user?.setPresence({
+        status: 'online',
+        activities: [
+          {
+            name: `FastGas: ${fastGas}`,
+            type: "PLAYING"
+          }
+        ]
+      });
+    })
+    .catch(error => console.log(error.message));
 };
 
 
